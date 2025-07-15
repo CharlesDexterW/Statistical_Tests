74bbfab880eb865ea026f665ea489a8defa45493
Benjamin Garcés <72029816+CharlesDexterW@users.noreply.github.com>
2025-07-15 13:32:46 -0500

    Update Plot_&_ANOVA.R
    Corrected an while retrieving the data from the document. 
    
    Corrected conclusions.
    
    Added Post-Hoc analysis.

--- Plot_&_ANOVA.R
+++ Plot_&_ANOVA.R
@@ -6,17 +6,19 @@ library(readxl) ## I had to add this one alone anyway since the read_xlsx functi
 library(dplyr) ## this one I'll use it for later.
 library(ggplot2) ## needed to plot the graphs
 library(tidyr)
+library(pwr) ## to run a One way ANOVA.
 
 ## ---
 ## first let's load my data. This is real data from the lab I currently work at. 
 ## It corresponds spermic urine of four different Atelopus longirostris specimens collected by me.
 
-xlr <- read_xlsx("Cryoprotectant_Results.xlsx")
-## I've previously tidyed up this data on google worksheets.  But I'll name each column here.
+xlr <- read_xlsx("Cryoprotectant_Results.xlsx", col_names = FALSE)
+## I've previously tidyed up this data on google worksheets. 
+## Also, I used col_names so it doesn't use the first row as column names.
 
 colnames(xlr) <- c("Treatment", "Time_Lapse", "Motility")
 
-## BTW There's only two treatments which are cryoprotectant formulations prepared by my colleague. 
+## BTW There's only two treatments which are cryoprotectant formulations prepared by me. 
 ## I'm creating two new tables from my data to work smoothly.
 cpa1r <- xlr %>%
   filter(Treatment == "CPA1")
@@ -71,3 +73,51 @@ print(summary(anova_cpa1))
 
 print("ANOVA for CPA2:")
 print(summary(anova_cpa2))
+
+## There seems to be a big difference between the standard deviations of Time 5 and the others CPA2. We'll check that later on. 
+## Now let's run a Two-way ANOVA to compare the motility results between treatments (CPA1 and CPA2),
+## where motility is the dependent variable and Treatment and Time_Lapse as the two independent variables.
+
+anova_two_way <- aov(Motility ~ Treatment * Time_Lapse, data = xlr)
+print(summary(anova_two_way))
+
+## there's no significant overall difference in average motility between treatments.
+## However, Time_Lapse significantly impacts motility, which means it changes over time regardles of which cryoprotectant is used.
+## Both formulations seem to experience similar time-dependend effects on motility.
+
+## Now let's create a new item called "model_cpa2" through the aov() function using CPA2.
+## to test which time lapse yields the most different results. 
+
+model_cpa2 <- aov(Motility ~ factor(Time_Lapse), data = cpa2r)
+
+## For a Post-Hoc Analysis, let's run a Tukey test evaluate the CPA2 efficacy over time. 
+TukeyHSD(model_cpa2)
+## This table above presents a pairwise comparison:
+## diff: It's the mean difference in motility between two time points being compared.
+## lwr and upr: These are the lower and upper bounds of the 95% confidence interval for the mean difference.
+## if it doesn't include zero, it's statistically significant.
+## p adj: It's the adjusted p-value for the pairwise comparison. This p-value has been corrected
+## for multiple comparisons, maintaining a controlled family-wise error rate. 
+## a p adj value less than 0.05 (or chosen alpha level9 indicates a statistically significant difference between the two time points.
+
+
+## Now there's a statistically significant decrease in motility on the times 5-15 comparison,
+## whicho suggests that sperm motility declines past time 5. 
+## This is corroborated in the comparison of Time 5-20, where sperm motility drops more significantly.
+
+
+## As you can see, these are preliminary results. It's still not clear if these results figure a biological significance, 
+## but it's clear the number of repetitions is not statistically significant.
+## However I'll run here a One way ANOVA to calculate the appropiate number of repetitions using the "pwr" package.
+
+# n = number of subjects per group
+# k = number of groups
+# f = effect size (according ot Cohen's f it'll be equal to 0.25)
+# sig.level = it's the alpha level
+# power = desired power.
+
+pwr.anova.test(k = 4, f = 0.25, sig.level = 0.05, power = 0.80)
+
+
+## With this result I've got a better idea of an adequate number of assay repetitions 
+## we require to gather reliable results in this experiment.  
